11a0ba900b97c7f6bfa84b885474be7305e955bf
hassaneldash <hassan.eldash@yahoo.com>
2024-03-01 15:37:53 -0500

    App Template Creation

---
+++ todo_app/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Custom Todo App",
+    'summary': "Custom Todo App module",
+    'description': "Lab 1 - Odoo",
+    'aurthor': 'Hassan Mohamed ELDash',
+    'category': "Productivity",
+    'version': "17.0.0.1.0",
+    'depends': ['base'],
+    'application': True,
+    'installable': True,
+    'data': [
+        # 'views/ticket.xml',
+        # 'menus/ticket.xml',
+    ]
+}
